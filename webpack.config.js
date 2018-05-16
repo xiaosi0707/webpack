403e5eb2e224878caccba9618634f70c0bc99315
wyunfei <583514368@qq.com>
2018-05-16 23:01:12 +0800

    html-webpack-plugin插件的使用

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
 /*Created by SmallFour on 2018/5/16*/
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     devtool: 'eval-source-map',
     entry: __dirname + '/app/main.js', //唯一入口文件
     output: {
-        path: __dirname + '/public', // 打包后文件存放地方
+        path: __dirname + '/build', // 打包后文件存放地方
         filename: 'bundle.js'
     },
     devServer: {
-        contentBase: './public', // 服务器指向的目录
+        contentBase: './build', // 服务器指向的目录
         historyApiFallback: true,
         inline: true
     },
@@ -39,6 +40,9 @@ module.exports = {
         ]
     },
     plugins: [
-        new webpack.BannerPlugin('smallFour所有，翻版必究')
+        new webpack.BannerPlugin('smallFour所有，翻版必究'),
+        new HtmlWebpackPlugin({
+            template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
+        })
     ]
 }
