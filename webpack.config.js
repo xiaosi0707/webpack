432ec99fec5ed291ed87a5720ae97a7f87f9f136
wyunfei <583514368@qq.com>
2018-05-16 19:15:49 +0800

    配置服务器

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 /*Created by SmallFour on 2018/5/16*/
 module.exports = {
+    devtool: 'eval-source-map',
     entry: __dirname + '/app/main.js', //唯一入口文件
     output: {
         path: __dirname + '/public', // 打包后文件存放地方
         filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './public', // 服务器指向的目录
+        historyApiFallback: true,
+        inline: true
     }
 }
