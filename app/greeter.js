dcd4ceb470bc8d9d98f51ed86e9389687c1bd47b
wyunfei <583514368@qq.com>
2018-05-16 22:43:59 +0800

    class冲突解决

--- app/greeter.js
+++ app/greeter.js
@@ -1,6 +1,8 @@
+import styles from './greeter.css'; // 导入
 var config = require('./config.json');
 module.exports = () => {
     let greet = document.createElement('div');
+    greet.setAttribute('class', styles.test) // 设置class -> greeter__test--1F6gy
     greet.textContent = config.greetText
     return greet;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,11 @@ module.exports = {
                         loader: 'style-loader'
                     },
                     {
-                        loader: 'css-loader'
+                        loader: 'css-loader',
+                        options: {
+                            modules: true, // 指定启用css modules
+                            localIdentName: '[name]__[local]--[hash:base64:5]' // 指定css的类名格式
+                        }
                     }
                 ]
             }
